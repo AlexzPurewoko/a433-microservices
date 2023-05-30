03d6b79286fb126be49b87365bcb89a28951e73f
AlexzPurewoko <purwoko908@gmail.com>
2023-05-30 15:52:16 +0700

    Adding comments on build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Build a container with a specified image name and tags for v1.
+# This build using a default Dockerfile configuration
 docker build -t item-app-project-alexzforger -t item-app-project-alexzforger:v1 .
 
+# Check the list of images that have been attached
 docker image ls
 
+# Adding new tag for container image for making it possible for deploying into github container registry
 docker tag item-app-project-alexzforger:v1 ghcr.io/alexzpurewoko/item-app-project-alexzforger:v1
 
+# Login with github container registry
 docker login ghcr.io 
 
+# Push the image into github container registry with specified tag
 docker push ghcr.io/alexzpurewoko/item-app-project-alexzforger:v1
